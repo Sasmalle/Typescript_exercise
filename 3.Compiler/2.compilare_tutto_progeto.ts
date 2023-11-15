251f9de335c57da1bb20add501df117d3c3ce5ef
Sasmalle <alessandro.piccolo30@gmail.com>
2023-11-15 08:48:22 +0100

    update

--- 3.Compiler/2.compilare_tutto_progeto.ts
+++ 3.Compiler/2.compilare_tutto_progeto.ts
@@ -1,1 +1,3 @@
-//se ho più file ts per compilarli tutti insieme invece di fare più watch mode, inizializzo il progetto come progetto typescript: "tsc --init"
+/*se ho più file ts per compilarli tutti insieme invece di fare più watch mode, 
+inizializzo il progetto come progetto typescript: "tsc --init".
+Questo creerà un file tsconfig.json, se vi è questo file sappiamo che il compilatore di ts gestisce questo progetto*/
