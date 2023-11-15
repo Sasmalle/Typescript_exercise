04e6f7ee0f267907eb65d52fd21c2924906485db
Sasmalle <alessandro.piccolo30@gmail.com>
2023-11-15 10:39:53 +0100

    update

--- 5.Interfaccie/1.interfaccie.ts
+++ 5.Interfaccie/1.interfaccie.ts
@@ -34,3 +34,102 @@ class y implements Querty, Prova {
     scrivi(): void {
     }
 }
+
+//esempio
+
+
+interface Internet {
+    ip: number;
+    connettiInternet(): void;
+}
+abstract class Dispositivo{
+    constructor(protected nome:string, protected anno:number){}
+
+    abstract accendi():void;
+    abstract spegni():void;
+}
+
+class Telefono extends Dispositivo{
+    constructor(nome:string,anno:number){
+        super(nome,anno);
+    }
+    accendi(): void {
+        console.log("telefono accensione..");
+    }
+    spegni(): void {
+        console.log("telefono spegnimento..");
+    }
+
+}
+
+class Smartphone extends Dispositivo implements Internet{
+    ip: number;
+
+    constructor(nome:string,anno:number,ip:number){
+        super(nome,anno)
+        this.ip = ip;
+    }
+    accendi(): void {
+        console.log("smartphone accensione..");
+    }
+    connettiInternet(): void {
+        console.log("smartphone connessione ad internet..");
+    }
+    spegni(): void {
+        console.log("smartphone spegnimento..");
+    }
+}
+
+class Computer extends Dispositivo implements Internet{
+    ip: number;
+
+    constructor(nome:string,anno:number,ip:number){
+        super(nome,anno)
+        this.ip = ip;
+    }
+    accendi(): void {
+        console.log("computer accensione..");
+    }
+    connettiInternet(): void {
+        console.log("computer connessione ad internet..");
+    }
+    spegni(): void {
+        console.log("computer spegnimento..");
+    }
+}
+
+//interfaccie che estendono più interfaccie
+
+interface X{
+    nome: string;
+}
+interface Y extends X{
+    cognome:string;
+    respira():void;
+}
+interface Z{
+    telefono: string;
+    saluta():void;
+}
+
+interface W extends Y,Z{
+    via:string;
+    mangia():void;
+}
+
+class Person implements W{
+    via: string;
+    cognome: string;
+    nome: string;
+    telefono: string;
+    mangia(): void {
+        console.log("mangia");
+    }
+    respira(): void {
+        console.log("respira");
+    }
+    saluta(): void {
+        console.log("saluta");
+    }
+
+}
