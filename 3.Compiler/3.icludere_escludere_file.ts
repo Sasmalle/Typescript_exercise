f819c884d0cf05794b8cc9ed15390436f5e0ccb8
Sasmalle <alessandro.piccolo30@gmail.com>
2023-11-15 08:56:27 +0100

    update

---
+++ 3.Compiler/3.icludere_escludere_file.ts
@@ -0,0 +1,3 @@
+/*si utilizzano i comandi "exclude" ed "include" dentro il file tsconfig.json.
+Come paramtro deve avere un arry di percorso (nel quale inseriamo il percorso del file da includere o escludere
+*/
---
+++ 3.Compiler/4.target_compilazione.ts
@@ -0,0 +1,2 @@
+/*dentro il tsconfig.json c'è un attributo target che indica la versione del compilatore (es2016),
+utilizzato principalemte per runnare in vecchi browser*/
